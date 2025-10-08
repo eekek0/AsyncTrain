7ddabb3def276d4b0d1f440b068210bdf90d5d34
Eekek <modsenPractice@gmail.com>
2025-10-08 17:11:39 +0300

    DateTime заменен на Stopwatch

--- Async/Async.cs
+++ Async/Async.cs
@@ -1,49 +1,50 @@
-﻿class Program
+﻿using System.Diagnostics;
+
+class Program
+{
+    static string ProcessData(string dataName)
     {
-        static string ProcessData(string dataName)
-        {
-            var startSync = DateTime.Now;
-            Thread.Sleep(3000);
-            var endSync = DateTime.Now;
-            return $"Обработка '{dataName}' завершена за  {(endSync - startSync).TotalSeconds} секунды";
-        }
-
-        static async Task<string> ProcessDataAsync(string dataName)
-        {
-            var startAsync = DateTime.Now;
-            await Task.Delay(3000);
-            var endAsync = DateTime.Now;
-        return $"Обработка '{dataName}' завершена за  {(endAsync - startAsync).TotalSeconds} секунды";
-
-        }
-
-        static async Task Main(string[] args)
-        {
-            Console.WriteLine("Синхронная обработка");
-            var startSync = DateTime.Now;
-
-            Console.WriteLine(ProcessData("Файл 1"));
-            Console.WriteLine(ProcessData("Файл 2"));
-            Console.WriteLine(ProcessData("Файл 3"));
-
-            var endSync = DateTime.Now;
-            Console.WriteLine($"Синхронная обработка заняла: {(endSync - startSync).TotalSeconds} сек.\n");
-
-            Console.WriteLine("Асинхронная обработка");
-            var startAsync = DateTime.Now;
-
-            var task1 = ProcessDataAsync("Файл 1");
-            var task2 = ProcessDataAsync("Файл 2");
-            var task3 = ProcessDataAsync("Файл 3");
-
-            var results = await Task.WhenAll(task1, task2, task3);
-
-            foreach (var result in results)
-                {
-                    Console.WriteLine(result);
-                }
-
-        var endAsync = DateTime.Now;
-            Console.WriteLine($"Асинхронная обработка заняла: {(endAsync - startAsync).TotalSeconds} сек.");
-        }
+        var sw = Stopwatch.StartNew();
+        Thread.Sleep(3000);
+        sw.Stop();
+        return $"Обработка '{dataName}' завершена за {sw.Elapsed.TotalSeconds} секунды";
     }
+
+    static async Task<string> ProcessDataAsync(string dataName)
+    {
+        var sw = Stopwatch.StartNew();
+        await Task.Delay(3000);
+        sw.Stop();
+        return $"Обработка '{dataName}' завершена за {sw.Elapsed.TotalSeconds} секунды";
+    }
+
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Синхронная обработка");
+        var swSync = Stopwatch.StartNew();
+
+        Console.WriteLine(ProcessData("Файл 1"));
+        Console.WriteLine(ProcessData("Файл 2"));
+        Console.WriteLine(ProcessData("Файл 3"));
+
+        swSync.Stop();
+        Console.WriteLine($"Синхронная обработка заняла: {swSync.Elapsed.TotalSeconds} сек.\n");
+
+        Console.WriteLine("Асинхронная обработка");
+        var swAsync = Stopwatch.StartNew();
+
+        var task1 = ProcessDataAsync("Файл 1");
+        var task2 = ProcessDataAsync("Файл 2");
+        var task3 = ProcessDataAsync("Файл 3");
+
+        var results = await Task.WhenAll(task1, task2, task3);
+
+        foreach (var result in results)
+        {
+            Console.WriteLine(result);
+        }
+
+        swAsync.Stop();
+        Console.WriteLine($"Асинхронная обработка заняла: {swAsync.Elapsed.TotalSeconds} сек.");
+    }
+}
